f152f9d52f512b67443cc97fd7dc463ff2e3e67f
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-03 16:22:42 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -6,12 +6,12 @@ let a = [{
     + 'VERSION:3.0\n'
     + 'N:;Ownerbot;;;FN:~ ʙᴏᴛᴢ ~\n'
     + 'ORG:Developer\n'
-    + 'TEL;Creator Dan Developer 👑;waid=6283844009539:6283844009539\n'
+    + 'TEL;Creator👑;waid=6285822347348:6285822347348\n'
     //+ 'item1.TEL;Support Whatsapp;waid=15517868060:15517868060\n'
     + `item1.X-ABLabel:👑 Creator ${global.wm} \n`
-    + 'item2.EMAIL;type=INTERNET:xiuan@gmail.com\n'
+    + 'item2.EMAIL;type=INTERNET: arulltes@gmail.com\n'
     + 'item2.X-ABLabel:💌 𝙀𝙢𝙖𝙞𝙡\n'
-    + 'item3.URL:https://instagram.com/_dev\n'
+    + 'item3.URL:https://instagram.com/sahrulwara_____\n'
     + 'item3.X-ABLabel:Instagram\n'
     + 'item4.ADR:;;🇲🇨 Indonesia 🇲🇨;;;;\n'
     + 'item4.X-ABADR:ac\n'
@@ -21,21 +21,35 @@ let a = [{
   },
   {
    displayName: 'Halo',
-   vcard: 'BEGIN:VCARD\n' // metadata of the contact card
-   + 'VERSION:3.0\n' 
-   + 'N:;Manusia;;;'
-   + 'FN:~ orang ~\n' // full name
-   + 'ORG:Pengembang\nTEL;Pengembang & Moderator ♨️;waid=994407430641:994407430641\n'
-   //+ 'item1.TEL;Support Whatsapp;waid=6281299878753:6281299878753\n' // WhatsApp ID + phone number
+   vcard: 'BEGIN:VCARD\n' // metadata of the contact card
+
+   + 'VERSION:3.0\n' 
+
+   + 'N:;Manusia;;;'
+
+   + 'FN:~ orang ~\n' // full name
+
+   + 'ORG:Pengembang\nTEL;Bot ♨️;waid=6283843192208:6283843192208\n'
+
+   //+ 'item1.TEL;Support Whatsapp;waid=6281299878753:6281299878753\n' // WhatsApp ID + phone number
+
    + `item1.X-ABLabel:👑 Moderator ${global.wm} \n`
-   + 'item2.EMAIL;type=INTERNET:ryumd@gmail.com\n'
-   + 'item2.X-ABLabel:💌 𝙀𝙢𝙖𝙞𝙡\n'
-   //+ 'item3.URL:https://instagram.com/_13\n'
-   //+ 'item3.X-ABLabel:Instagram\n'
-   + 'item4.ADR:;;🇲🇨 Indonesian 🇲🇨;;;;\n'
-   + 'item4.X-ABADR:ac\n'
-   + 'item4.X-ABLabel:🌎 𝙍𝙚𝙜𝙞𝙤𝙣\n'
-   + 'item5.X-ABLabel:Chat Yg Penting Saja\n'
+   + 'item2.EMAIL;type=INTERNET: arulltes@gmail.com\n'
+
+   + 'item2.X-ABLabel:💌 𝙀𝙢𝙖𝙞𝙡\n'
+
+   //+ 'item3.URL:https://instagram.com/sahrulwara_____\n'
+
+   //+ 'item3.X-ABLabel:Instagram\n'
+
+   + 'item4.ADR:;;🇲🇨 Indonesian 🇲🇨;;;;\n'
+
+   + 'item4.X-ABADR:ac\n'
+
+   + 'item4.X-ABLabel:🌎 𝙍𝙚𝙜𝙞𝙤𝙣\n'
+
+   + 'item5.X-ABLabel:Gusah Dichat,Gbakaln Dibalas\n'
+
    + 'END:VCARD'
 }];
 
