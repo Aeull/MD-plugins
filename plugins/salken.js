9fb83078bc6984024283a2a8496e401f212efaca
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-04 17:55:50 +0800

    Update salken.js

--- plugins/salken.js
+++ plugins/salken.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
-	conn.sendButton(m.chat, `Halo Kak👋\nSaya adalah ShinoaBot\nShinoaBot adalah Sebuah Bot yang bisa membantumu di grup ini, klik tombol dibawah ini jika kamu ingin menggunakan bot!`.trim(), global.wm, '⋮☰ Menu', '.menu', m)
+	conn.sendButton(m.chat, `Halo Kak👋\nSaya adalah ArullBotz\nArullBotz adalah Sebuah Bot yang bisa membantumu di grup ini, klik tombol dibawah ini jika kamu ingin menggunakan bot!`.trim(), global.wm, '⋮☰ Menu', '.menu', m)
 	}
 
 handler.command = /^(salken)$/i
