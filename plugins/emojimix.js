cec972f388284227dfcfc03143095a98f45fef66
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-04 22:37:54 +0800

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -1,18 +1,18 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, text }) => {
-		try {
-			let emo = text.split('+')
-			if (!emo[0]) return conn.reply(m.chat, 'Emojinya?\nContoh: .emojimix 😪+😴', m)
-			if (!emo[1]) return conn.reply(m.chat, 'Contoh:\n!emojimix 😪+😴', m)
-			let res = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emo[0])}_${encodeURIComponent(emo[1])}`)).json()
-			conn.sendImageAsSticker(m.chat, res.results[0].url, m, { packname: 'Emojimix', author: `Made By ${wm}` })
-		} catch (e) {
-			console.log(e)
-			conn.reply(m.chat, 'Emoji tidak support!!\nSilakan ganti salah satu emoji!', m)
-	}
+let {fetchJson} = require("../lib/myfunc")
+let fs = require("fs")
+
+let handler = async (m, { conn, text, args }) => {
+if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🐷&😣*'
+   	let [emoji1, emoji2] = text.split`&`
+		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
+		for (let res of anu.results) {
+		    let encmedia = await conn.sendImageAsSticker(m.chat, res.url, m, { packname: global.packname, author: global.author, categories: res.tags })
+		    await fs.unlinkSync(encmedia)
+		}
 }
-handler.help = ['emojimix', 'semix'].map((v) => v + ' <emoji>')
-handler.tags = ['internet', 'sticker']
-handler.command = /^(emojimix|semix)$/i
+handler.help = ['emojimix'].map(v => v + ' emot1|emot2>')
+handler.tags = ['fun']
+handler.command = /^(emojimix)$/i
 
 module.exports = handler
